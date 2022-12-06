5ffdae255d20c73bf8805dbfdd12d44cd8a90fde
Davhho-dev <Davhho@utexas.edu>
2022-12-05 23:27:41 -0600

    Remove restart button

--- script.js
+++ script.js
@@ -4,13 +4,9 @@ const Player = (name, symbol, currentPlayer) => {
 
 const gameBoard = (() => {
     const cells = document.querySelectorAll(".cell");
+    const restart = document.querySelector("button");
+    const playerWin = document.querySelector(".player-win");
     let board = ["", "", "", "", "", "", "", "", ""];
-
-    const reset = () => {
-        cells.forEach((cell) => {
-            cell.innerHTML = "";
-        })
-    }
     
     const update = () => {
         cells.forEach((cell) => {
@@ -34,7 +30,7 @@ const gameBoard = (() => {
             })
         });
     }
-    return {reset, update, board};
+    return {update, board};
 })();
 
 const displayController = (() => {
@@ -70,22 +66,21 @@ const displayController = (() => {
         }
     };
 
-    let winner = () => {
+    const winner = () => {
        winCombination.forEach((index) => {
             if(gameBoard.board[index[0]] === "X" && gameBoard.board[index[1]] === "X" && gameBoard.board[index[2]] === "X") {
                 winDeclared = true;
-                console.log(gameBoard.board[index[0]] + gameBoard.board[index[1]] + gameBoard.board[index[2]]);
                 playerWin.innerHTML = "Player One Wins";
                 playerWin.style.color = "rgb(0, 132, 255)";
             }else if(gameBoard.board[index[0]] === "O" && gameBoard.board[index[1]] === "O" && gameBoard.board[index[2]] === "O") {
                 winDeclared = true;
                 playerWin.innerHTML = "Player Two Wins";
                 playerWin.style.color = "rgb(255, 79, 79)";
-            };
+            }
         });
         return winDeclared;
     };
-    return {current, winner};
+    return {current, winner, winDeclared};
 })();
 
 const gameController = (() => {
@@ -105,64 +100,3 @@ const gameController = (() => {
 
 return{current, playerOne, playerTwo};
 })();
-
-// const gameBoard = (() => {
-//     const playerOne = Player("p1", "X", true);
-//     const playerTwo = Player("p2", "O", false);
-//     let board = ["", "", "", "", "", "", "", "", ""];
-//     let round = 1;
-//     const winCombination = [
-//         [0, 1, 2],
-//         [3, 4, 5],
-//         [6, 7, 8],
-//         [0, 3, 6],
-//         [1, 4, 7],
-//         [2, 5, 8],
-//         [0, 4, 8],
-//         [2, 4, 6]
-//     ];
-
-//     const p1 = document.getElementById("p1");
-//     p1.setAttribute("style", "font-size: 4rem; color: rgb(0, 132, 255)");
-//     const p2 = document.getElementById("p2");
-//     const playRound = (() => {
-//         const cells = document.querySelectorAll(".cell");
-//         cells.forEach((cell) => {
-//             cell.addEventListener("click", (e) => {
-//                 if(playerOne.currentPlayer && cell.innerHTML == "") {
-//                     board[e.target.id] = playerOne.symbol;
-//                     cell.innerHTML = playerOne.symbol;
-//                     cell.style.color = "rgb(0, 132, 255)";
-//                     playerOne.currentPlayer = false;
-//                     playerTwo.currentPlayer = true;
-//                     console.log(board);
-//                     round++;
-//                     if(round <= 9) {
-//                         p1.setAttribute("style", "font-size: 3.5rem; color: black");
-//                         p2.setAttribute("style", "font-size: 4rem; color: rgb(255, 79, 79)");
-//                     }else {
-//                         p1.setAttribute("style", "font-size: 3.5rem; color: black");
-//                         p2.setAttribute("style", "font-size: 3.5rem; color: black");
-//                     }
-//                 }else if(playerTwo.currentPlayer && cell.innerHTML == "") {
-//                     board[e.target.id] = playerTwo.symbol;
-//                     cell.innerHTML = playerTwo.symbol;
-//                     cell.style.color = "rgb(255, 79, 79)";
-//                     playerTwo.currentPlayer = false;
-//                     playerOne.currentPlayer = true;
-//                     console.log(board);
-//                     round++;
-//                     if(round <= 9) {
-//                         p2.setAttribute("style", "font-size: 3.5rem; color: black");
-//                         p1.setAttribute("style", "font-size: 4rem; color: rgb(0, 132, 255)");
-//                     }else {
-//                         p2.setAttribute("style", "font-size: 3.5rem; color: black");
-//                         p1.setAttribute("style", "font-size: 3.5rem; color: black");
-//                     }
-//                 }else return;
-//             })
-//         });
-//     })();
-
-//     return {playRound, playerOne, playerTwo, round};   
-// })();
